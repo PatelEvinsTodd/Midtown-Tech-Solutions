9465f844186fabdc861fe1d539f5fccf27ab2030
Tanner Evins <tanner@tannerevins.com>
2014-07-27 17:54:23 -0500

    Add Article model and admin registration

--- mts/public/admin.py
+++ mts/public/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from public.models import Article
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['title']
+
+admin.site.register(Article, ArticleAdmin)
--- mts/public/models.py
+++ mts/public/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Article(models.Model):
+    pub_date = models.DateField()
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
