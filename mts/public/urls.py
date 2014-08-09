55780bb92099e3b11781374aafe02c8cb097d914
Tanner Evins <tanner@tannerevins.com>
2014-08-09 08:45:19 -0500

    Add filler urls, views, and templates

--- mts/public/urls.py
+++ mts/public/urls.py
@@ -3,4 +3,7 @@ import public.views
 
 urlpatterns = patterns('',
     url(r'^$', public.views.index, name="index"),
+    url(r'^about$', public.views.about, name="about"),
+    url(r'^organization$', public.views.people, name="people"),
+    url(r'^contact$', public.views.contact, name="contact"),
 )
--- mts/public/views.py
+++ mts/public/views.py
@@ -4,4 +4,16 @@ from public.models import Article
 
 def index(request):
     context = {}
-    return render(request, 'public/base.html', context)
+    return render(request, 'public/index.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'public/about.html', context)
+
+def people(request):
+    context = {}
+    return render(request, 'public/people.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'public/contact.html', context)
